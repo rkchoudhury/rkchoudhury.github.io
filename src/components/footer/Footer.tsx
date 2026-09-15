fd645007be74dda160552b56508836c657a2578a
rkchoudhury <rakeshchoudhury.rocky@gmail.com>
2026-09-15 16:40:10 +0530

    Added Visitor count

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,20 +2,22 @@ import { useState, useMemo } from 'react';
 import { profile } from '../../configs';
 import packageJson from "../../../package.json";
 import "./style.css";
+import { useVisitorCount } from '../../hooks/useVisitorCount';
 
 const Footer = () => {
     const [count, setCount] = useState(1);
+    const visitorCount = useVisitorCount();
 
     const appVersion = useMemo(() => packageJson.version, []);
     const label = useMemo(() => {
-        return count > 5 ? `App: v${appVersion}` : ` ${profile.name}`
+        return count > 5 ? `App: v${appVersion}, Visitors: ${visitorCount}` : `© ${new Date().getFullYear()} ${profile.name}`
     }, [count]);
 
     return (
         <footer>
             <div className="container footer-inner">
                 <p onClick={() => { setCount(value => value + 1) }}>
-                    © {new Date().getFullYear()} {label}
+                    {label}
                 </p>
                 <p onClick={() => { setCount(1) }}>"Build. Learn. Improve."</p>
                 <p>Made with ❤️ in India</p>
